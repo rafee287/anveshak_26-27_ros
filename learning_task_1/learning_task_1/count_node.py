f409d9f41157d54a520b546bc7c8e888316122d8
rafee287 <rafee2587@gmail.com>
2026-03-30 16:56:17 +0530

    added the learning task_1 codes

--- learning_task_1/learning_task_1/count_node.py
+++ learning_task_1/learning_task_1/count_node.py
@@ -1,3 +1,44 @@
+# #!/usr/bin/env python3
+# import rclpy 
+# from rclpy.node import Node
+# from std_msgs.msg import Int8
+
+# class CountNode(Node):
+    
+#     def __init__(self):
+#         super().__init__("counter_node")
+#         self.get_logger().info("count_node started")
+#         self.number_subscriber = self.create_subscription(Int8,"/number",self.number_recieved_callback, 10)
+#         self.counter = 0
+#         self.count_publisher = self.create_publisher(Int8,"/count",10)
+#         # self.numbers = {}
+#         self.current_number = None 
+
+#     def number_recieved_callback(self, msg:Int8):
+
+#         # if msg.data not in self.numbers.keys() :
+#         #     self.numbers[msg] = 0
+#         # else:
+#         #     self.numbers[msg] +=1
+
+#         received_val = msg.data 
+#         if received_val == self.current_number:
+#             self.counter += 1
+#         else:
+#             self.current_number = received_val
+#             self.counter = 1
+#         self.get_logger().info("counter_value: " + str(self.counter) + "and the number recived is " + str(msg.data))
+#         publish_value = Int8()
+#         publish_value.data = self.counter
+#         self.count_publisher.publish(publish_value)
+#         self.get_logger().info("the number published is (count):" + str(publish_value.data))
+
+# def main(args = None):
+#     rclpy.init(args = args)
+#     node = CountNode()
+#     rclpy.spin(node)
+#     rclpy.shutdown()
+
 #!/usr/bin/env python3
 import rclpy 
 from rclpy.node import Node
@@ -8,18 +49,16 @@ class CountNode(Node):
     def __init__(self):
         super().__init__("counter_node")
         self.get_logger().info("count_node started")
-        self.number_subscriber = self.create_subscription(Int8,"/number",self.number_recieved_callback, 10)
+        
+        self.number_subscriber = self.create_subscription(Int8, "/number", self.number_recieved_callback, 10)
+        self.count_publisher = self.create_publisher(Int8, "/count", 10)
+        
         self.counter = 0
-        self.count_publisher = self.create_publisher(Int8,"/count",10)
-        # self.numbers = {}
         self.current_number = None 
+        
+        self.timer = self.create_timer(0.5, self.publish_timer_callback)
 
-    def number_recieved_callback(self, msg:Int8):
-
-        # if msg.data not in self.numbers.keys() :
-        #     self.numbers[msg] = 0
-        # else:
-        #     self.numbers[msg] +=1
+    def number_recieved_callback(self, msg: Int8):
 
         received_val = msg.data 
         if received_val == self.current_number:
@@ -27,14 +66,20 @@ class CountNode(Node):
         else:
             self.current_number = received_val
             self.counter = 1
-        self.get_logger().info("counter_value: " + str(self.counter) + "and the number recived is " + str(msg.data))
-        publish_value = Int8()
-        publish_value.data = self.counter
-        self.count_publisher.publish(publish_value)
-        self.get_logger().info("the number published is (count):" + str(publish_value.data))
+            
+        self.get_logger().info("counter_value: " + str(self.counter) + " and the number received is " + str(msg.data))
 
-def main(args = None):
-    rclpy.init(args = args)
+    def publish_timer_callback(self):
+
+        if self.current_number is not None:
+            publish_value = Int8()
+            publish_value.data = self.counter
+            self.count_publisher.publish(publish_value)
+            self.get_logger().info("the number published is (count):" + str(publish_value.data))
+
+def main(args=None):
+    rclpy.init(args=args)
     node = CountNode()
     rclpy.spin(node)
     rclpy.shutdown()
+
--- learning_task_1/learning_task_1/number_node.py
+++ learning_task_1/learning_task_1/number_node.py
@@ -1,46 +1,88 @@
+# #!/usr/bin/env python3
+# import rclpy 
+# from rclpy.node import Node
+# from std_msgs.msg import Int8
+
+# class NumberNode (Node):
+    
+#     def __init__(self):
+#         super().__init__("number_node")
+#         self.get_logger().info("number_node started")
+#         self.start_timer = self.create_timer(1,self.time_less_than_zero)
+#         self.number_subscriber = self.create_subscription(Int8,"/count",self.count_recieved_callback, 10)
+#         self.n = 0
+#         self.count_publisher = self.create_publisher(Int8,"/number",10)
+
+#     def count_recieved_callback(self,msg:Int8):
+#         value = Int8()
+#         self.get_logger().info("recieved count: " + str(msg.data))
+
+#         # if self.n > 5:
+#         #     while (1):
+#         #         pass 
+
+#         if self.n == 0 and msg.data == 1:
+#             msg.data = 0
+
+#         if msg.data == self.n:
+#             self.n +=1
+        
+#         value.data = self.n 
+#         self.count_publisher.publish(value)
+#         self.get_logger().info("sending value: " + str(value.data))
+    
+#     def time_less_than_zero(self):
+#         value = Int8()
+#         value.data = self.n
+#         self.get_logger().info("publishing value " +str(value.data) + " because t<0")
+#         self.count_publisher.publish(value)
+#         if self.count_publisher.get_subscription_count() > 0:
+#             self.start_timer.cancel()
+
+# def main(args = None):
+#     rclpy.init(args = args)
+#     node = NumberNode()
+#     rclpy.spin(node)
+#     rclpy.shutdown()
+
 #!/usr/bin/env python3
 import rclpy 
 from rclpy.node import Node
 from std_msgs.msg import Int8
 
-class NumberNode (Node):
+class NumberNode(Node):
     
     def __init__(self):
         super().__init__("number_node")
         self.get_logger().info("number_node started")
-        self.start_timer = self.create_timer(1,self.time_less_than_zero)
-        self.number_subscriber = self.create_subscription(Int8,"/count",self.count_recieved_callback, 10)
-        self.n = 0
-        self.count_publisher = self.create_publisher(Int8,"/number",10)
-
-    def count_recieved_callback(self,msg:Int8):
-        value = Int8()
-        self.get_logger().info("recieved count: " + str(msg.data))
-
-        if self.n > 5:
-            while (1):
-                pass 
-
-        if self.n == 0 and msg.data == 1:
-            msg.data = 0
-
-        if msg.data == self.n:
-            self.n +=1
         
+        self.number_subscriber = self.create_subscription(Int8, "/count", self.count_recieved_callback, 10)
+        self.count_publisher = self.create_publisher(Int8, "/number", 10)
+        
+        self.n = 0
+        
+        self.timer = self.create_timer(0.5, self.publish_timer_callback)
+
+    def count_recieved_callback(self, msg: Int8):
+
+        received_val = msg.data
+        self.get_logger().info("received count: " + str(received_val))
+
+        if self.n == 0 and received_val == 1:
+            received_val = 0
+
+        if received_val == self.n:
+            self.n += 1
+            
+    def publish_timer_callback(self):
+
+        value = Int8()
         value.data = self.n 
         self.count_publisher.publish(value)
         self.get_logger().info("sending value: " + str(value.data))
-    
-    def time_less_than_zero(self):
-        value = Int8()
-        value.data = self.n
-        self.get_logger().info("publishing value" +str(value.data) + "because t<0")
-        self.count_publisher.publish(value)
-        if self.count_publisher.get_subscription_count() > 0:
-            self.start_timer.cancel()
 
-def main(args = None):
-    rclpy.init(args = args)
+def main(args=None):
+    rclpy.init(args=args)
     node = NumberNode()
     rclpy.spin(node)
     rclpy.shutdown()
